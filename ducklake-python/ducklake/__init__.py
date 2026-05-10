0c0077b2bd97a82e5ac46af4b55a60be790fced8
Oliver Borchert <me@borchero.com>
2026-05-10 21:51:14 +0200

    build: Fix PyPI package name

--- ducklake-python/ducklake/__init__.py
+++ ducklake-python/ducklake/__init__.py
@@ -2,9 +2,9 @@ import importlib.metadata
 import warnings
 
 try:
-    __version__ = importlib.metadata.version(__name__)
+    __version__ = importlib.metadata.version("ducklake-sdk")
 except importlib.metadata.PackageNotFoundError as e:  # pragma: no cover
-    warnings.warn(f"Could not determine version of {__name__}\n{e!s}", stacklevel=2)
+    warnings.warn(f"Could not determine version of ducklake-sdk\n{e!s}", stacklevel=2)
     __version__ = "unknown"
 
 from .connect import connect, create
