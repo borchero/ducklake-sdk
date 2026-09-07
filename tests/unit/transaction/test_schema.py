f07d35754a2e45cde435e4c734740da7317d09e7
Moritz Potthoff <moritz.potthoff@quantco.com>
2026-09-07 22:32:31 +0200

    parametrize

--- tests/unit/transaction/test_schema.py
+++ tests/unit/transaction/test_schema.py
@@ -1,45 +1,4 @@
-import pytest
-
 import ducklake as dl
-import ducklake.exceptions as dlexc
-
-
-def test_delete_missing_schema_raises(
-    shared_ducklake: dl.Ducklake, random_schema_name: str
-) -> None:
-    # Act & Assert
-    with shared_ducklake.transaction() as tx:
-        with pytest.raises(dlexc.NotFoundError):
-            tx.delete_schema(random_schema_name)
-
-
-def test_delete_missing_schema_skip(
-    shared_ducklake: dl.Ducklake, random_schema_name: str
-) -> None:
-    # Arrange
-    snapshot = shared_ducklake.get_latest_snapshot()
-
-    # Act
-    with shared_ducklake.transaction() as tx:
-        tx.delete_schema(random_schema_name, if_not_exists="skip")
-
-    # Assert
-    assert random_schema_name not in shared_ducklake.list_schemas()
-    assert shared_ducklake.get_latest_snapshot().id == snapshot.id
-
-
-def test_delete_existing_schema_skip(
-    shared_ducklake: dl.Ducklake, random_schema_name: str
-) -> None:
-    # Arrange
-    shared_ducklake.create_schema(random_schema_name)
-
-    # Act
-    with shared_ducklake.transaction() as tx:
-        tx.delete_schema(random_schema_name, if_not_exists="skip")
-
-    # Assert
-    assert random_schema_name not in shared_ducklake.list_schemas()
 
 
 def test_create_delete_schema_does_nothing(
